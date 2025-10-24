a1412ae6a5e8d4461386ddc4d48c636592ca23cf
cabrel Ange <cabrel.lene@yahoo.com>
2025-10-24 02:56:22 -0400

    server/models/User.js

---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema(
+{
+name: { type: String, required: true, trim: true },
+email: { type: String, required: true, unique: true, lowercase: true },
+password: { type: String, required: true },
+avatar: { type: String, default: "" },
+bio: { type: String, default: "" },
+followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+},
+{ timestamps: true }
+);
+
+
+export const User = mongoose.model("User", userSchema);
